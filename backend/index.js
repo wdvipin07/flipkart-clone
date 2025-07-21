de854b026acffe26feb3167d639dbf43a938a31a
wdvipin07 <codervipinchaudhary@gmail.com>
2025-07-21 23:13:56 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -79,7 +79,7 @@ app.use("/", paymentRouter);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 // Server Listen
