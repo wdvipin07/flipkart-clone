41b5867bf2e05f7feb968a22a559e83b43a58e11
wdvipin07 <codervipinchaudhary@gmail.com>
2025-07-28 18:15:33 +0530

    update payment failure components

--- frontend/src/components/payment/PaymentFailure.jsx
+++ frontend/src/components/payment/PaymentFailure.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+import { Box, styled } from "@mui/material";
+
+const Conatainer = styled(Box)(({ theme }) => ({
+  textAlign: "center",
+  padding: "20px",
+  marginTop: 120,
+}));
+
 function PaymentFailure() {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
+    <Conatainer>
       <h1>🎉 Payment Failed!</h1>
       <p>Try Again</p>
-    </div>
+    </Conatainer>
   );
 }
 
