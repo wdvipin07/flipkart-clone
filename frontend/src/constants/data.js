a44fc426b6428ec104e9c43cb04000bac1efbfef
wdvipin07 <codervipinchaudhary@gmail.com>
2025-07-29 21:40:17 +0530

    update navbar image url

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -1,10 +1,5 @@
 export const NavData = [
   {
-    url: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-    text: "Grocery",
-    category: "Grocery",
-  },
-  {
     url: "https://rukminim2.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100",
     text: "Mobiles",
     category: "Smartphones",
@@ -30,14 +25,19 @@ export const NavData = [
     category: "Laptops",
   },
   {
-    url: "https://rukminim1.flixcart.com/flap/64/64/image/71050627a56b4693.png?q=100",
+    url: "https://static.vecteezy.com/system/resources/thumbnails/050/177/034/small_2x/smart-watch-isolated-on-transparent-background-png.png",
     text: "Watches",
     category: "Smart Watches",
   },
   {
     url: "https://rukminim1.flixcart.com/flap/64/64/image/dff3f7adcf3a90c6.png?q=100",
-    text: "Beuty, Toys & More",
+    text: "Beuty & Toys",
     category: "Beauty & toys",
+  },
+  {
+    url: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+    text: "Grocery",
+    category: "Grocery",
   },
 ];
 
